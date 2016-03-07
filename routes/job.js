e69a8260cac4e7203f077045d96337ad38fc1cbc
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-07 13:52:38 +0000

    Add (dummy) email address to job object

--- routes/job.js
+++ routes/job.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var queue_job = require('../lib/queue-job');
+var create_job = require('../lib/create-job');
 
 var uploader = multer({
   dest: __dirname + '/../uploads/'
@@ -11,8 +12,14 @@ router.post(
     '/',
     uploader.single('package'),
     function(req, res, next) {
-	queue_job(req);
-	res.end("OK");
+	create_job(req, function(err, job) {
+	    if (err) {
+		res.render("badpackage", { 'error': err });
+	    } else {
+		queue_job(job);
+		res.render("ok", job);
+	    }
+	})
     }
 )
 
