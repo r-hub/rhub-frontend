c3378cbd9842e62f5e14b2ca55aac3d56848ee36
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-29 07:49:31 -0700

    Add jobs for CRAN packages

--- routes/job.js
+++ routes/job.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var rhub = require('rhub-node');
 var queue_job = require('../lib/queue-job');
 var create_job = require('../lib/create-job');
+var create_cran_job = require('../lib/create-cran-job');
 
 var uploader = multer({
   dest: __dirname + '/../uploads/'
@@ -13,6 +15,21 @@ router.post(
     uploader.single('package'),
     function(req, res, next) {
 	create_job(req, function(err, job) {
+	    if (err) {
+		res.render("badpackage", { 'error': err });
+	    } else {
+		queue_job(job);
+		res.render("ok", job);
+	    }
+	})
+    }
+)
+
+router.post(
+    new RegExp('^/cran/(' + rhub.valid_package_name + ')$'),
+    function(req, res) {
+	var package = req.params[0];
+	create_cran_job(package, req, function(err, job) {
 	    if (err) {
 		res.render("badpackage", { 'error': err });
 	    } else {
