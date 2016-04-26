178b2ab5ee0863fc0ad63a42e5c89429101e70f4
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-26 16:20:10 +0100

    Set GH callback URL from env var

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ var dokkucheck = require('./routes/check');
 
 var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+var RHUB_BUILDER_URL = process.env.RHUB_BUILDER_URL
 
 passport.use(
     new GitHubStrategy(
 	{
 	    clientID: GITHUB_CLIENT_ID,
 	    clientSecret: GITHUB_CLIENT_SECRET,
-	    callbackURL: 'http://127.0.0.1:3000/login/github/callback'
+	    callbackURL: RHUB_BUILDER_URL + '/login/github/callback'
 	},
 	function(accessToken, refreshToken, profile, cb) {
 	    return cb(null, 'github:' + JSON.stringify(profile.emails));
