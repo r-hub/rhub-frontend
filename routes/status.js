6e37bafd11f0e09f9edf3fc9236b7fab95b6f738
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-24 12:56:09 +0100

    Better logs

--- routes/status.js
+++ routes/status.js
@@ -6,6 +6,7 @@ var JenkinsLogStream = require('jenkins-log-stream');
 var get_user = require('../lib/get-user');
 var byline = require('byline');
 var LogFilter = require('../lib/filter-log');
+var SimpleLogFilter = require('../lib/filter-log').SimpleLogFilter;
 
 var JENKINS_URL = process.env.JENKINS_URL;
 
@@ -41,8 +42,6 @@ router.get(new RegExp('^/' + re_status), function(req, res) {
     );
 });
 
-// This is the IFrame embedded into the main page
-
 router.get(new RegExp('^/log/' + re_status), function(req, res) {
     var name = req.params[0];
     var log = new JenkinsLogStream({
@@ -56,11 +55,35 @@ router.get(new RegExp('^/log/' + re_status), function(req, res) {
 	.write(
 	    "<!doctype html>" +
 	    "<html lang=en><head><meta charset=utf-8>" +
-	    "<link rel=\"stylesheet\" href=\"/stylesheets/logstyle.css\">" +
+	    "<link rel=\"stylesheet\" href=\"/stylesheets/style.css\">" +
 	    "</head><body>"
 	);
 
     log_by_line.pipe(logFilter).pipe(res)
+});
+
+router.get(new RegExp('^/embedded/' + re_status), function(req, res) {
+    var name = req.params[0];
+    var log = new JenkinsLogStream({
+	'baseUrl': JENKINS_URL,
+	'job': name
+    });
+    var log_by_line = byline(log);
+    var logFilter = new LogFilter();
+
+    log_by_line.pipe(logFilter).pipe(res)
+});
+
+router.get(new RegExp('^/raw/' + re_status), function(req, res) {
+    var name = req.params[0];
+    var log = new JenkinsLogStream({
+	'baseUrl': JENKINS_URL,
+	'job': name
+    });
+    var log_by_line = byline(log);
+    var simpleLogFilter = new SimpleLogFilter();
+
+    log_by_line.pipe(simpleLogFilter).pipe(res)
 });
 
 module.exports = router;
