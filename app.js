2efd387d4109b0bbe711d540940280adf00cc25c
Gábor Csárdi <csardi.gabor@gmail.com>
2018-05-02 10:38:14 +0100

    Starting to play with GH webhook

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var dokkucheck = require('./routes/check');
 var status = require('./routes/status');
 var api = require('./routes/api');
 var build = require('./routes/build');
+var project = require('./routes/project');
+
+require('dotenv').config();
 
 require('dotenv').config();
 
@@ -105,6 +108,8 @@ app.use('/build', build);
 app.use('/file', express.static('uploads'));
 
 app.use('/check', dokkucheck);
+
+app.use('/-/project', project);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
